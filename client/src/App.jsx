5a2377307b13d500dade7dd0009fdd2189ccad2f
RuiShengGit <seow.ruisheng@gmail.com>
2022-05-24 12:20:39 +0800

    Halfdone Register page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
 import Home from "./pages/home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Announcement from "./components/Announcement";
 
 const App = () => {
-  return <Home/>;
-};
+  return <>
+  <Announcement/>
+  <Navbar/>
+  <Home/>
+  <Footer/>
+  </>
 
+
+};
+ 
 export default App;
--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,22 +1,17 @@
 import React from 'react'
-import Announcement from '../components/Announcement'
 import Categories from '../components/Categories'
-import Navbar from '../components/Navbar'
-import Newsletter from '../components/Newsletter'
 import Slider from '../components/Slider'
-import Footer from '../components/Footer' 
+import Newsletter from '../components/Newsletter'
+
 
 
 
 const Home = () => {
   return (
     <div>
-       <Announcement/>
-       <Navbar/>
        <Slider/>
        <Categories/>
        <Newsletter/>
-       <Footer/>
     </div>
   )
 }
