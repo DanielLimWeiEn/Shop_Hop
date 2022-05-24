53489969f9b15ecf77da2e2e34214001a5fdcb99
Daniel Lim <DanielLimWeiEn@gmail.com>
2022-05-24 12:40:33 +0800

    Removed MongoDB URI

--- server/server.js
+++ server/server.js
@@ -2,11 +2,12 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
+import dotenv from "dotenv";
 
 import userRoutes from './routes/users.js';
 
-
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -15,7 +16,7 @@ app.use(cors());
 app.use('/user', userRoutes);
 
 const PORT = 5000;
-const CONNECTION_URL = 'mongodb+srv://Daniel:Daniel@shophop.ykuep.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_URL = process.env.CONNECTION_URL;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
