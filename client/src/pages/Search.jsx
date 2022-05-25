1b195645fb2ab9f515366bb1a0a6bb2f8a9e5ac5
RuiShengGit <seow.ruisheng@gmail.com>
2022-05-25 13:23:53 +0800

    Made minor design changes to shopping page

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,22 +1,49 @@
 import styled from "styled-components"
+import { Search } from "@mui/icons-material";
 
 const Container = styled.div`
-
+  width: 90vw;
+  height: 90vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 20px;
 `
 
-const SearchBar = styled.input`
+const SearchContainer = styled.div`
+  border: 0.5px solid gray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
 
-`
+const Input = styled.input`
+  border: none;
+  width: 400px;
+`;
+
 const Title = styled.h1`
    
 `
-const Search = () => {
+const Button = styled.button`
+    display: flex;
+    align-items: center;
+    background transparent;
+    border: 0.5px solid gray;
+`;
+const Searching = () => {
   return (
     <Container>
-      <SearchBar/>
-      <Title></Title>
+      <SearchContainer>
+            <Input />
+            <Button>
+                <Search style={{ color: "gray", fontSize: 16 }} />
+            </Button>
+       </SearchContainer>
+      <Title>This is a shopping site</Title>
     </Container>
   )
 }
 
-export default Search
+export default Searching
