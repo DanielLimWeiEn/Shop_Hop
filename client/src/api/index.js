afc0c9c4d17b69b4f03a9c5fd6682b96b89ff6d3
Daniel Lim <DanielLimWeiEn@gmail.com>
2022-06-08 13:46:13 +0800

    Fix placing of history componenets:

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -19,3 +19,10 @@ export const signUp = (formData) => API.post("/user/signup", formData);
 
 export const showShopping = () => API.get("/shopping");
 export const generateShopping = (formData) => API.post("/shopping/search", formData);
+
+export const showProfile = () => API.get("/profile");
+export const showPurchaseHistory = () => API.get("/profile/history");
+export const showPurchases = () => API.get("/profile/manage");
+export const addPurchase = (formData) => API.post("/profile/manage", formData);
+export const updatePurchase = (id, formData) => API.put(`/profile/manage/${id}`, formData);
+export const deletePurchase = (id) => API.delete(`/profile/manage/${id}`);
--- client/src/components/History.jsx
+++ client/src/components/History.jsx
@@ -1,77 +1,80 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 const Container = styled.div`
-   width: 85vw;
-   height: 100vh;
-   display:flex;
-   flex-direction: column;
-`
+  box-sizing: border-box;
+  width: 85vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  gap: 20px;
+`;
 
 const Top = styled.div`
   display: flex;
-  justify-content:center;
-  align-items center;
-  gap: 50px;
-  height: 450px;
+  justify-content: space-between;
+  align-items: center;
+  gap: 20px;    
+  height: 50%;
   width: 100%;
-`
+`;
 
 const Bottom = styled.div`
- display: flex;
-  justify-content:center;
-  align-items center;
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 50%;
+  width: 100%;
+`;
 
 const Summary = styled.div`
-   height: 400px;
-   width: 700px;
-   border: 1px solid;
-`
-const Image = styled.img`
-   width : 400px;
-   height: 350px
-`
+  height: 100%;
+  width: 50%;
+  border: 1px solid;
+`;
+// const Image = styled.img`
+//   width: 400px;
+//   height: 350px;
+// `;
 
 const Spending = styled.div`
-    height: 400px;
-    width: 500px;
-    border: 1px solid;
-`
+  height: 100%;
+  width: 50%;
+  border: 1px solid;
+`;
 
 const ItemList = styled.div`
-   height: 350px;
-   width: 1250px;
-   border: 1px solid;
-`
+  height: 100%;
+  width: 100%;
+  border: 1px solid;
+`;
 
 const ItemHeading = styled.div`
-    font-size: 25px;
-    color: white;
-    padding-left: 20px;
-    padding-top: 5px;
-    background-color: purple;
-    height: 40px;
-`
+  font-size: 25px;
+  color: white;
+  padding-left: 20px;
+  padding-top: 5px;
+  background-color: purple;
+  height: 40px;
+`;
 
 const History = () => {
   return (
-      <Container>
-          <Top>
-              <Summary>
-                  Graph
-                  <Image src = "https://www.conceptdraw.com/solution-park/resource/images/solutions/bar-graphs/Graphs-and-Charts-Bar-Charts-Percentage-with-Various-Levels-of-Family-Income.png"/>
-              </Summary>
-              <Spending>
-                 Your spending total is a morbillion dollars
-              </Spending>
-          </Top>
-          <Bottom>
-              <ItemList>
-                  <ItemHeading>Items Purchased</ItemHeading>
-              </ItemList>
-          </Bottom>
-      </Container>
-  )
-}
+    <Container>
+      <Top>
+        <Summary>
+          Graph
+          {/* <Image src="https://www.conceptdraw.com/solution-park/resource/images/solutions/bar-graphs/Graphs-and-Charts-Bar-Charts-Percentage-with-Various-Levels-of-Family-Income.png" /> */}
+        </Summary>
+        <Spending>Your spending total is a morbillion dollars</Spending>
+      </Top>
+      <Bottom>
+        <ItemList>
+          <ItemHeading>Items Purchased</ItemHeading>
+        </ItemList>
+      </Bottom>
+    </Container>
+  );
+};
 
-export default History
+export default History;
