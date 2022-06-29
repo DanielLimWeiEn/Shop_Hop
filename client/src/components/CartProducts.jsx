c28b115ee2e77b1ee4748e163246774d263d4fc4
Daniel Lim <DanielLimWeiEn@gmail.com>
2022-06-29 13:34:20 +0800

    Improve UI design of Cart page, specifically the cart Summary page and cart layout

--- client/src/components/CartProducts.jsx
+++ client/src/components/CartProducts.jsx
@@ -2,36 +2,56 @@ import styled from "styled-components";
 import { Add, Remove } from "@mui/icons-material";
 
 const Product = styled.div`
+  height: 30vh;
+  width: 100%;
+  box-sizing: border-box;
   display: flex;
   justify-content: space-between;
+  border: 0.5px solid lightgray;
+  border-radius: 10px;
 `;
 
 const ProductDetail = styled.div`
-  flex: 2;
   display: flex;
+  height: 100%;
+  width: 70%;
 `;
 
 const Image = styled.img`
-  width: 200px;
+  height: 100%;
+  width: 35%;
+  min-width: 35%;
+  border-top-left-radius: 10px;
+  border-bottom-left-radius: 10px;
+  object-fit: cover;
 `;
 
 const Details = styled.div`
+  box-sizing: border-box;
   padding: 20px;
   display: flex;
   flex-direction: column;
   justify-content: space-around;
 `;
 
-const ProductName = styled.span``;
+const ProductName = styled.span`
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
 
-const ProductId = styled.span``;
+const ProductId = styled.span`
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
 
 const PriceDetail = styled.div`
-  flex: 1;
+  height: 100%;
+  width: 30%;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  background-color: blue;
 `;
 
 const ProductAmountContainer = styled.div`
